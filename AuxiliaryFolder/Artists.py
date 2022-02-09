7844cd8b9378910a808da206c2fa114635948e49
LiorCohen1 <Liorcohen122003@gmail.com>
2022-02-09 15:40:22 +0200

    Update is_exists func

--- AuxiliaryFolder/Artists.py
+++ AuxiliaryFolder/Artists.py
@@ -8,6 +8,6 @@ class Artists:
     def add_artist(self, artist):
         self.artists.append(artist)
 
-    def get_artist(self, id, artist):
-        location = AuxiliaryFunctions.get_location(AuxiliaryFunctions, id, artist)
+    def get_artist(self, id, artists):
+        location = AuxiliaryFunctions.get_location(AuxiliaryFunctions, id, artists.artists)
         return self.artists[location]
