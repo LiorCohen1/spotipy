d7850bfc081b35ba5d7a6c09443dfe209b7c79e2
LiorCohen1 <Liorcohen122003@gmail.com>
2022-02-09 15:42:52 +0200

    Adding add song into album func

--- Classes/Album.py
+++ Classes/Album.py
@@ -1,3 +1,6 @@
+from AuxiliaryFolder.AuxiliaryFunctions import *
+
+
 class Album:
     def __init__(self, id_code, album_name):
         self.id = id_code
@@ -10,3 +13,8 @@ class Album:
     def get_songs(self):
         return self.songs
 
+    def add_song_into_album(self, album, artist, song):
+        location = AuxiliaryFunctions.get_location(AuxiliaryFunctions, album.id, artist.albums)
+        if location != -1:
+            Album.add_song(artist.albums[location], song)
+            return artist
