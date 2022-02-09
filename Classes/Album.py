d8387468a6903bc2c5d7014798577964556483c9
LiorCohen1 <Liorcohen122003@gmail.com>
2022-02-09 11:32:51 +0200

    Update variable names

--- Classes/Album.py
+++ Classes/Album.py
@@ -1,10 +1,11 @@
 class Album:
-    def __init__(self, id_number):
-        self.id = id_number
+    def __init__(self, id_code, album_name):
+        self.id = id_code
+        self.name = album_name
         self.songs = []
 
     def add_song(self, song):
-        self.songs.insert(song)
+        self.songs.append(song)
 
     def get_songs(self):
         return self.songs
