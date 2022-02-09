964e3faf1350c0100b394e3a6650ef037f963f94
LiorCohen1 <Liorcohen122003@gmail.com>
2022-02-09 15:41:46 +0200

    Update is_exists func

--- AuxiliaryFolder/AuxiliaryFunctions.py
+++ AuxiliaryFolder/AuxiliaryFunctions.py
@@ -1,23 +1,18 @@
 class AuxiliaryFunctions:
     def is_exists(self, check_item, items):
-        for item in items:
-            if item.id == check_item.id:
+
+        for i in range(len(items) - 1):
+            if items[i].id == check_item.id:
                 return True
-            else:
-                return False
+
+            return False
 
     def get_location(self, id, items):
-        counter = 0
-        for cell in items:
-            if cell.id == id:
+        for counter in range(len(items)):
+            if items[counter].id == id:
                 return counter
-            else:
-                counter = counter + 1
-
-    def add_song_into_album(self, album, artist, song):
-        location = AuxiliaryFunctions.get_location(AuxiliaryFunctions, album.id, artist.albums)
-        artist.albums[location].add_song(song)
-        return artist
+        else:
+            return -1
 
     def get_id(self, item):
         return item.id
