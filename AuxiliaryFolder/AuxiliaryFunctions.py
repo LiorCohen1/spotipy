6ddf3bf5fcbe483b6e4f5cc8da7a99f5842c3992
LiorCohen1 <Liorcohen122003@gmail.com>
2022-02-09 22:39:11 +0200

    adding two new functions- one is for printing all the songs, and the other is for return a song object

--- AuxiliaryFolder/AuxiliaryFunctions.py
+++ AuxiliaryFolder/AuxiliaryFunctions.py
@@ -16,3 +16,20 @@ class AuxiliaryFunctions:
 
     def get_id(self, item):
         return item.id
+
+    def print_songs(self, stock):
+        for artist in stock.artists:
+            for album in artist.albums:
+                for song in album.songs:
+                    print(" song name : " + song.name)
+                    print(" song id " + song.id)
+
+    def get_song(self, stock, id):
+        for artist in stock.artists:
+            for album in artist.albums:
+                for song in album.songs:
+                    if song.id == id:
+                        return song
+
+        print("Song dose not exists")
+        return None
