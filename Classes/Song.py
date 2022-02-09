61ec8c370ec7f7cb02cc00296ef7c52feb77a90e
LiorCohen1 <Liorcohen122003@gmail.com>
2022-02-09 11:33:38 +0200

    Update variable names

--- Classes/Song.py
+++ Classes/Song.py
@@ -1,10 +1,10 @@
 class Song:
-    def __init__(self, id_number):
-        self.id = id_number
-        self.popularity = 0
+    def __init__(self, id_code, song_popularity, song_name):
+        self.id = id_code
+        self.popularity = song_popularity
+        self.name = song_name
 
-    def set_popularity(self):
-        self.popularity = self.popularity + 1
+
 
     def get_popularity(self):
         return self.popularity
