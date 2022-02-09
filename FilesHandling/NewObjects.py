6ae349badc3862e1cc713f6b4fe50f79f6e56d2a
LiorCohen1 <Liorcohen122003@gmail.com>
2022-02-09 15:44:23 +0200

    Made the func return artists,artist

--- FilesHandling/NewObjects.py
+++ FilesHandling/NewObjects.py
@@ -1,25 +1,28 @@
 from ReadFromFile import *
-from AuxiliaryFolder.AuxiliaryFunctions import AuxiliaryFunctions
+from AuxiliaryFolder.AuxiliaryFunctions import *
 from Classes.Artist import Artist
 
 
 class NewObjects:
     def add_new_objects(self, path, artists):
         song = ReadFromFile.new_song(ReadFromFile, path)
-        song_artists = (ReadFromFile.get_artist(ReadFromFile, path))
+        song = Song(song.id, song.popularity, song.name)
+        song_artists = (ReadFromFile.create_artists(ReadFromFile, path))
 
         album = (ReadFromFile.get_album(ReadFromFile, path))
         for artist in song_artists:
-            if AuxiliaryFunctions.is_exists(AuxiliaryFunctions, artist, artists):
-                if AuxiliaryFunctions.is_exists(AuxiliaryFunctions, album, artists.get_artist(Artist.get_id(artist))):
+            if AuxiliaryFunctions.is_exists(AuxiliaryFunctions, artist, artists.artists):
+                if AuxiliaryFunctions.is_exists(AuxiliaryFunctions, album, artists.get_artist(Artist.get_id(artist),artists).get_albums()):
                     AuxiliaryFunctions.add_song_into_album(AuxiliaryFunctions, album, artist, song)
 
 
                 else:
-                    artist.add_album(artist, album)
-                    AuxiliaryFunctions.add_song_into_album(AuxiliaryFunctions, album, artist, song)
+                    Artist.add_album(artist, album)
+                    Album.add_song_into_album(Album, album, artist, song)
             else:
-                artists.append(artist)
+                artists.add_artist(artist)
                 artist.add_album(album)
-                AuxiliaryFunctions.add_song_into_album(AuxiliaryFunctions, album, artist, song)
+                Album.add_song_into_album(Album, album, artist, song)
         return artists, artist
+
+
