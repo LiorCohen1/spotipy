61a048abef32225036207358d16129f65199564b
LiorCohen1 <Liorcohen122003@gmail.com>
2022-02-10 11:32:00 +0200

    Added an exception case

--- FilesHandling/UploadingFiles.py
+++ FilesHandling/UploadingFiles.py
@@ -4,8 +4,12 @@ from FilesHandling.NewObjects import *
 
 class UploadingFiles:
     def upload(self, folder_path, artists):
-        for path in os.listdir(folder_path):
-            full_path = os.path.join(folder_path, path)
-            if os.path.isfile(full_path):
-                artists, artist = NewObjects.add_new_objects(NewObjects, full_path, artists)
-        return artists
+        try:
+            for path in os.listdir(folder_path):
+                full_path = os.path.join(folder_path, path)
+                if os.path.isfile(full_path):
+                    artists = NewObjects.add_new_objects(NewObjects, full_path, artists)
+            return artists
+        except FileNotFoundError:
+            print("Wrong path")
+            return False
