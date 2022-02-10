f1c933921bfee689ea9e0e62388402408c156f63
LiorCohen1 <Liorcohen122003@gmail.com>
2022-02-10 11:32:37 +0200

    Moved to Classes Folder

--- UsersPlaylists/PlaylistCreation.py
+++ UsersPlaylists/PlaylistCreation.py
@@ -1,6 +1,6 @@
 from UsersPlaylists.PlaylistFunctions import PlaylistFunctions
-from UsersPlaylists.Playlist import Playlist
-from UsersPlaylists.Playlists import Playlists
+from Classes.Playlist import Playlist
+from Classes.Playlists import Playlists
 from AuxiliaryFolder.AuxiliaryFunctions import AuxiliaryFunctions
 
 
@@ -10,15 +10,9 @@ class PlaylistCreation:
         if PlaylistFunctions.is_name_valid(PlaylistCreation, name, playlists, author) == True:
             playlist = Playlist(name, author)
             AuxiliaryFunctions.print_songs(AuxiliaryFunctions, stock)
-            id = input("Please enter a song ID to add it to this playlist. To finish adding the songs, enter Finish")
-            while id != "Finish":
-                song = AuxiliaryFunctions.get_song(AuxiliaryFunctions, stock, id)
-                if song != None:
-                    playlist.add_song(song)
-                id = input(
-                    "Please enter a song ID to add it to this playlist. To finish adding the songs, enter Finish")
+            PlaylistFunctions.add_song_into_playlist(PlaylistFunctions, playlist, stock)
 
-            Playlists.add_new_paylist(playlists, playlist)
+            Playlists.add_new_playlist(playlists, playlist)
 
         else:
             print("The name is already in use")
--- UsersPlaylists/PlaylistFunctions.py
+++ UsersPlaylists/PlaylistFunctions.py
@@ -1,3 +1,6 @@
+from AuxiliaryFolder.AuxiliaryFunctions import AuxiliaryFunctions
+
+
 class PlaylistFunctions:
     def get_name(self):
         return input("Please enter a name for your new playlist")
@@ -8,3 +11,12 @@ class PlaylistFunctions:
                 if playlist.author == author:
                     return False
         return True
+
+    def add_song_into_playlist(self, playlist, stock):
+        id = input("Please enter a song ID to add it to this playlist. To finish adding the songs, enter Finish")
+        while id != "Finish":
+            song = AuxiliaryFunctions.get_song(AuxiliaryFunctions, stock, id)
+            if song != None:
+                playlist.add_song(song)
+            id = input(
+                "Please enter a song ID to add it to this playlist. To finish adding the songs, enter Finish")
